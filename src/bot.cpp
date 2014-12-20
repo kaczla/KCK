1f29fe7ff3214cc5f29bb327882cc228ff5706b2
kaczla <kaczla@wp.pl>
2014-12-20 11:52:18 +0100

    Przetwarzanie tekstu dla AIML

--- src/bot.cpp
+++ src/bot.cpp
@@ -45,6 +45,66 @@ std::string Bot::ReturnAnswer( std::string text ){
 }
 
 std::string Bot::ReturnAnswer( u16string text ){
+	for( unsigned int i=0; i<text.size(); i++ ){
+		switch( text[i] ){
+		case 0x104:
+			text[i] = 'A';
+			break;
+		case 0x15A:
+			text[i] = 'S';
+			break;
+		case 0x106:
+			text[i] = 'C';
+			break;
+		case 0x118:
+			text[i] = 'E';
+			break;
+		case 0xD3:
+			text[i] = 'O';
+			break;
+		case 0x141:
+			text[i] = 'L';
+			break;
+		case 0x143:
+			text[i] = 'N';
+			break;
+		case 0x17B:
+			text[i] = 'Z';
+			break;
+		case 0x179:
+			text[i] = 'Z';
+			break;
+		case 0x105:
+			text[i] = 'a';
+			break;
+		case 0x15B:
+			text[i] = 's';
+			break;
+		case 0x107:
+			text[i] = 'c';
+			break;
+		case 0x119:
+			text[i] = 'e';
+			break;
+		case 0xF3:
+			text[i] = 'o';
+			break;
+		case 0x142:
+			text[i] = 'l';
+			break;
+		case 0x144:
+			text[i] = 'n';
+			break;
+		case 0x17C:
+			text[i] = 'z';
+			break;
+		case 0x17A:
+			text[i] = 'z';
+			break;
+		default:
+			break;
+		}
+	}
 	std::string tmp_text ( text.begin(), text.end() );
 	return this->ReturnAnswer( tmp_text );
 }
